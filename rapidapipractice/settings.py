8a8f68108d10957d110ba53e5dc7cbb7d03d9358
Chibuzor Obiora <218098562@student.uj.ac.za>
2022-10-14 03:05:09 +0200

    Update settings.py

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -138,3 +138,7 @@ INSTALLED_APPS = [
 ‘api’,
   ‘rest_framework’,
 ]
+
+# Heroku: Update database configuration from $DATABASE_URL. 
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
